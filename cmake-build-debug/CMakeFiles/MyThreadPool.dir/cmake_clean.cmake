05e6c29e53dd20020a1ac0780c457905a0cf53f3
wangqiang <760206156@qq.com>
2025-05-06 16:30:54 +0800

    2025.5.6

---
+++ cmake-build-debug/CMakeFiles/MyThreadPool.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyThreadPool.dir/threadpool.cpp.o"
+  "CMakeFiles/MyThreadPool.dir/threadpool.cpp.o.d"
+  "CMakeFiles/MyThreadPool.dir/线程池项目测试.cpp.o"
+  "CMakeFiles/MyThreadPool.dir/线程池项目测试.cpp.o.d"
+  "MyThreadPool"
+  "MyThreadPool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyThreadPool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
